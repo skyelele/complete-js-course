07272660436efb35191e6ca913a50f0998c6849a
Skye Leahy <45550323+skyelele@users.noreply.github.com>
2019-04-11 22:19:32 -0700

    finish section 5 and start on budget project :)

--- Complete-JavaScript-Course/5-section/challenge_1_redogurl.js
+++ Complete-JavaScript-Course/5-section/challenge_1_redogurl.js
@@ -4,18 +4,18 @@
     this.answers = answers;
     this.correct = correct;
   }
-  
+
   Question.prototype.displayQuestion = function() {
     console.log(this.question);
-  
+
     for (let i = 0; i < this.answers.length; i++) {
       console.log(i + ": " + this.answers[i]);
     }
   };
-  
+
   Question.prototype.checkAnswer = function(ans, callback) {
     function (ans) {
-      const sc;
+      const src;
 
       if (ans === this.correct) {
         console.log('Correct answer!');
@@ -33,26 +33,26 @@
     console.log('Your current score is: ' + score);
     console.log('---------------------------------');
   }
-  
+
   const q1 = new Question(
     "Is JS the coolest programming language in the world?",
     ["Yes", "No"],
     0
   );
-  
+
   const q2 = new Question(
     "What is the name of this course's teacher?",
     ["John", "Michael", "Jonas"],
     2
   );
-  
+
   const q3 = new Question(
     "What does best describe coding?",
     ["Boring", "Hard", "Fun", "Tedious"],
     2
   );
 
-  const questions = [q1, q2, q3];  
+  const questions = [q1, q2, q3];
 
   function score() {
     const sc = 0;
@@ -67,21 +67,20 @@
   const keepScore = score();
 
   function nextQuestion() {
-    
+
     const n = Math.floor(Math.random() * questions.length);
-    
+
     questions[n].displayQuestion();
-    
+
     const answer = prompt("Please select the correct answer.");
 
     if(answer !== 'exit') {
-      questions[n].checkAnswer(parseInt(answer), keepScore);
+      questions[n].checkAnswer(parseInt(answer, keepScore));
 
       nextQuestion();
     }
   }
 
   nextQuestion();
-  
-})();
 
+})();
